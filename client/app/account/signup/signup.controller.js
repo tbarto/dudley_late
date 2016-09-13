031fa19b457f685a3cb0c7eafa61bb52941386ce
johndeyrup <john.deyrup@gmail.com>
2016-09-13 19:15:10 -0400

    commit for pull

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -1,52 +1,57 @@
 'use strict';
 
-class SignupController {
-  //end-non-standard
 
-  constructor(Auth, $state, $http) {
-      this.Auth = Auth;
-      this.$state = $state;
-      this.$http = $http;
-      this.stopData = [];
-      // ToDo this needs to be set a collection
-      this.schools = ['School 1', 'School 2', 'School 3'];
-    }
-    //start-non-standard
-
-  $onInit() {
-    this.$http.get('/api/things')
-      .then(response => {
-        this.awesomeThings = response.data;
-      });
-  }
-
-  register(form) {
-    this.submitted = true;
-    if (form.$valid) {
-      this.Auth.createUser({
-          name: this.user.name,
-          email: this.user.email,
-          password: this.user.password,
-          school: this.user.school,
-          studentId: this.user.userId
-        })
-        .then(() => {
-          // Account created, redirect to home
-          this.$state.go('main');
-        })
-        .catch(err => {
-          err = err.data;
-          this.errors = {};
-
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, (error, field) => {
-            form[field].$setValidity('mongoose', false);
-            this.errors[field] = error.message;
-          });
-        });
-    }
-  }
-}
+  // class SignupController {
+  //
+  //   //end-non-standard
+  //
+  //   constructor(Auth, $state, $http) {
+  //     this.Auth = Auth;
+  //     this.$state = $state;
+  //     this.$http = $http;
+  //     this.stopData = [];
+  //     // ToDo this needs to be added as a collection
+  //     this.schools = ['School 1', 'School 2', 'School 3'];
+  //   }
+  //
+  //   //start-non-standard
+  //
+  //   $onInit() {
+  //     this.$http.get('/api/trainRoutes')
+  //       .then(response => {
+  //         this.trainRoutes = response.data;
+  //       });
+  //   }
+  //
+  //   register(form) {
+  //     this.submitted = true;
+  //     if (form.$valid) {
+  //       this.Auth.createUser({
+  //         name: this.user.name,
+  //         email: this.user.email,
+  //         password: this.user.password,
+  //         school: this.user.school,
+  //         studentId: this.user.userId
+  //       })
+  //         .then(() => {
+  //           // Account created, redirect to home
+  //           this.$state.go('main');
+  //         })
+  //         .catch(err => {
+  //           err = err.data;
+  //           this.errors = {};
+  //
+  //           // Update validity of form fields that match the mongoose errors
+  //           angular.forEach(err.errors, (error, field) => {
+  //             form[field].$setValidity('mongoose', false);
+  //             this.errors[field] = error.message;
+  //           });
+  //         });
+  //     }
+  //   }
+  // }
 
 angular.module('tTimeApp')
-  .controller('SignupController', SignupController);
+  .controller('SignupController', function ($scope, $state, $http) {
+
+  });
--- server/routes.js
+++ server/routes.js
@@ -13,8 +13,7 @@ export default function(app) {
   app.use('/api/users', require('./api/user'));
   app.use('/api/train', require('./api/train'));
   app.use('/api/trainRoutes', require('./api/trainRoutes'));
-
-  // app.use('/auth', require('./auth').default);
+  app.use('/auth', require('./auth').default);
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
